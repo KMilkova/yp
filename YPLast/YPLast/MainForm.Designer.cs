e1df222bb6f9aa157c84cbb63fe521f7c7d91a0a
KMilkova <nastyavb14@gmail.com>
2019-12-13 08:06:26 +0300

    rrr

---
+++ YPLast/YPLast/MainForm.Designer.cs
@@ -0,0 +1,370 @@
+﻿namespace YPLast
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.yPProjectDataSet = new YPLast.YPProjectDataSet();
+            this.textBoxId = new System.Windows.Forms.TextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxProducts = new System.Windows.Forms.TextBox();
+            this.textBoxRecipe = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBoxCategory = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBoxDelete = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBoxUpdateRecipe = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yPProjectDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(819, 328);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // bindingSource1
+            // 
+            this.bindingSource1.DataSource = this.yPProjectDataSet;
+            this.bindingSource1.Position = 0;
+            // 
+            // yPProjectDataSet
+            // 
+            this.yPProjectDataSet.DataSetName = "YPProjectDataSet";
+            this.yPProjectDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // textBoxId
+            // 
+            this.textBoxId.Location = new System.Drawing.Point(1014, 15);
+            this.textBoxId.Name = "textBoxId";
+            this.textBoxId.Size = new System.Drawing.Size(100, 26);
+            this.textBoxId.TabIndex = 1;
+            this.textBoxId.TextChanged += new System.EventHandler(this.textBoxId_TextChanged);
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(1014, 65);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(100, 26);
+            this.textBoxName.TabIndex = 2;
+            this.textBoxName.TextChanged += new System.EventHandler(this.textBoxId_TextChanged);
+            // 
+            // textBoxProducts
+            // 
+            this.textBoxProducts.Location = new System.Drawing.Point(1014, 120);
+            this.textBoxProducts.Name = "textBoxProducts";
+            this.textBoxProducts.Size = new System.Drawing.Size(100, 26);
+            this.textBoxProducts.TabIndex = 3;
+            this.textBoxProducts.TextChanged += new System.EventHandler(this.textBoxId_TextChanged);
+            // 
+            // textBoxRecipe
+            // 
+            this.textBoxRecipe.Location = new System.Drawing.Point(1014, 171);
+            this.textBoxRecipe.Name = "textBoxRecipe";
+            this.textBoxRecipe.Size = new System.Drawing.Size(100, 26);
+            this.textBoxRecipe.TabIndex = 4;
+            this.textBoxRecipe.TextChanged += new System.EventHandler(this.textBoxId_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(917, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Индекс";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(919, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Название";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(917, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Продукты";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(917, 177);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Рецепт";
+            // 
+            // comboBoxCategory
+            // 
+            this.comboBoxCategory.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxCategory.FormattingEnabled = true;
+            this.comboBoxCategory.Items.AddRange(new object[] {
+            "Десерт",
+            "Мясное",
+            "Рыбное",
+            "Салат"});
+            this.comboBoxCategory.Location = new System.Drawing.Point(1014, 223);
+            this.comboBoxCategory.Name = "comboBoxCategory";
+            this.comboBoxCategory.Size = new System.Drawing.Size(121, 28);
+            this.comboBoxCategory.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(919, 226);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Категория";
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(956, 275);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 29);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Добавление";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(683, 473);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(183, 31);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Удаление";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBoxDelete
+            // 
+            this.textBoxDelete.Location = new System.Drawing.Point(698, 424);
+            this.textBoxDelete.Name = "textBoxDelete";
+            this.textBoxDelete.Size = new System.Drawing.Size(138, 26);
+            this.textBoxDelete.TabIndex = 14;
+            this.textBoxDelete.TextChanged += new System.EventHandler(this.textBoxDelete_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(1030, 473);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(162, 31);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Изменить рецепт";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBoxUpdateRecipe
+            // 
+            this.textBoxUpdateRecipe.Location = new System.Drawing.Point(1137, 424);
+            this.textBoxUpdateRecipe.Name = "textBoxUpdateRecipe";
+            this.textBoxUpdateRecipe.Size = new System.Drawing.Size(138, 26);
+            this.textBoxUpdateRecipe.TabIndex = 16;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(947, 424);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(138, 26);
+            this.textBox1.TabIndex = 17;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(1115, 390);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(160, 20);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Изменение рецепта";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(943, 390);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(160, 20);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Имя для изменения";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(694, 390);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(194, 20);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "Название для удаления";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(421, 473);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(194, 31);
+            this.button4.TabIndex = 21;
+            this.button4.Text = "Поиск по категориям";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Десерт",
+            "Мясное",
+            "Рыбное",
+            "Салат"});
+            this.comboBox1.Location = new System.Drawing.Point(446, 424);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 28);
+            this.comboBox1.TabIndex = 22;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(97, 408);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(318, 96);
+            this.richTextBox1.TabIndex = 23;
+            this.richTextBox1.Text = "";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(97, 507);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(109, 30);
+            this.button5.TabIndex = 24;
+            this.button5.Text = "Очистить";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1580, 540);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBoxUpdateRecipe);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBoxDelete);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBoxCategory);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxRecipe);
+            this.Controls.Add(this.textBoxProducts);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.textBoxId);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "MainForm";
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yPProjectDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private YPProjectDataSet yPProjectDataSet;
+        private System.Windows.Forms.TextBox textBoxId;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxProducts;
+        private System.Windows.Forms.TextBox textBoxRecipe;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBoxCategory;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBoxDelete;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBoxUpdateRecipe;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ YPLast/YPLast/MainForm.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YPLast
+{
+    public partial class MainForm : Form
+    {
+        DataSet ds;
+        DataTable dt;
+        SqlCommand command;
+        SqlDataAdapter adapter;
+        SqlTransaction transaction;
+        SqlCommandBuilder builder;
+        
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+
+        string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=YPProject;Integrated Security=True";
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            comboBoxCategory.SelectedIndex= 0;
+            comboBox1.SelectedIndex = 0;
+            string sql = "SELECT * FROM CookBookSecond";
+            adapter = new SqlDataAdapter(sql, connectionString);
+            builder = new SqlCommandBuilder(adapter);
+            ds = new DataSet();
+            dt = new DataTable();
+            adapter.Fill(ds);
+
+            bindingSource1.DataSource = ds.Tables[0];
+            dataGridView1.DataSource = bindingSource1;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    transaction = connection.BeginTransaction();
+                    command = connection.CreateCommand();
+                    command.Transaction = transaction;
+
+                    command.CommandText = @"INSERT INTO CookBookSecond(Id, Name, Products, Recipe, Category) VALUES(@Id, @Name, @Products, @Recipe, @Category)";
+                    command.Parameters.AddWithValue("Id", Convert.ToInt32(textBoxId.Text));
+                    command.Parameters.AddWithValue("Name", textBoxName.Text);
+                    command.Parameters.AddWithValue("Products", textBoxProducts.Text);
+                    command.Parameters.AddWithValue("Recipe", textBoxRecipe.Text);
+                    command.Parameters.AddWithValue("Category", comboBoxCategory.SelectedItem.ToString());
+                    command.ExecuteNonQuery();
+                    transaction.Commit();
+
+                    dt = ds.Tables[0];
+                    DataRow row = dt.NewRow();
+                    row["Id"] = Convert.ToInt32(textBoxId.Text);
+                    row["Name"] = textBoxName.Text;
+                    row["Products"] = textBoxProducts.Text;
+                    row["Recipe"] = textBoxRecipe.Text;
+                    row["Category"] = comboBoxCategory.SelectedItem.ToString();
+                    dt.Rows.Add(row);
+
+                    ds.Clear();
+                    adapter.Fill(ds);
+                    dataGridView1.DataSource = ds.Tables[0];
+                }
+                catch (SqlException ex)
+                {
+                    transaction.Rollback();
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    transaction = connection.BeginTransaction();
+                    command = connection.CreateCommand();
+                    command.Transaction = transaction;
+
+                    command.CommandText = @"DELETE From CookBookSecond WHERE Name = @Name";
+                    command.Parameters.AddWithValue("Name", textBoxDelete.Text);
+                    command.ExecuteNonQuery();
+                    transaction.Commit();
+
+                    ds.Clear();
+                    adapter.Fill(ds);
+                    dataGridView1.DataSource = ds.Tables[0];
+                }
+                catch (SqlException ex)
+                {
+                    transaction.Rollback();
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    transaction = connection.BeginTransaction();
+                    command = connection.CreateCommand();
+                    command.Transaction = transaction;
+
+                    command.CommandText = @"UPDATE CookBookSecond SET Recipe = @Recipe WHERE Name = @Name";
+                    command.Parameters.AddWithValue("Name", textBox1.Text);
+                    command.Parameters.AddWithValue("Recipe", textBoxUpdateRecipe.Text);
+                    command.ExecuteNonQuery();
+                    transaction.Commit();
+
+                    ds.Clear();
+                    adapter.Fill(ds);
+                    dataGridView1.DataSource = ds.Tables[0];
+                }
+                catch (SqlException ex)
+                {
+                    transaction.Rollback();
+                }
+                finally
+                {
+                    connection.Close();
+                }
+
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+
+
+                    string sql = @"SELECT * FROM CookBookSecond WHERE Category=@Category";
+
+                    command = new SqlCommand(sql, connection);
+
+                    connection.Open();
+                    command.Parameters.AddWithValue("Category", comboBox1.SelectedItem.ToString());
+
+                    command.ExecuteNonQuery();
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        richTextBox1.AppendText("\nИндекс: " + reader[0].ToString() + "\nНазвание: " + reader[1].ToString() + "\nПродукты: " + reader[2].ToString() + "\nРецепт: " + reader[3].ToString() + "\nКатегория: " + reader[4].ToString() + "\n-------------------------------------------");
+                    }
+                    reader.Close();
+
+                    connection.Close();
+
+                }
+                catch (SqlException ex)
+                {
+
+                }
+                finally
+                {
+                    connection.Close();
+                }
+
+            }
+        }
+
+        private void textBoxId_TextChanged(object sender, EventArgs e)
+        {
+            if (textBoxId.Text != "" && textBoxName.Text != "" && textBoxProducts.Text != "" && textBoxRecipe.Text != "")
+            {
+                button1.Enabled = true;
+            }
+            else button1.Enabled=false;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "" && textBoxUpdateRecipe.Text != "")
+            {
+                button3.Enabled = true;
+            }
+            else button3.Enabled = false;
+        }
+
+        private void textBoxDelete_TextChanged(object sender, EventArgs e)
+        {
+            if (textBoxDelete.Text != "")
+            {
+                button2.Enabled = true;
+            }
+            else button2.Enabled = false;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+    }
+}
